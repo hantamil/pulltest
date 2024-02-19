b1e8ba41f35c0823d3a9d2e5b7900cc64080ac75
hantamil <mira0004@stud.kea.dk>
2024-02-19 13:03:24 +0100

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Andrea";
+        System.out.println(name);
+    }
+}
---
+++ src/main/java/com/example/pulltest/PulltestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pulltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulltestApplication.class, args);
+    }
+
+}
